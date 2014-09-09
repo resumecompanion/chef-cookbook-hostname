cfe2b24bd1223087d4655f1bea99a337b31b9843
nathan <nathan@vertile.com>
2014-09-09 11:02:13 -0700

    bump version

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ maintainer_email 'nathan@vertile.com'
 license          'MIT'
 description      'Configures hostname and FQDN'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.4'
+version          '0.3.5'
 
 supports 'debian'
 supports 'ubuntu'
